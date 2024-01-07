4387217e58b14c076808f0f2e11647f808a32452
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-01-06 19:23:19 -0500

    corrigiendo error de importancion de componente excperience

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import About from '../components/About'
 import Contact from '../components/Contact'
 import Main from '../components/Main'
 import Projects from '../components/Projects'
-import Experience from '../components/experience'
 import Skills from '../components/Skills'
+import Experience from '../components/Experience'
 
 
 export default function Home() {
