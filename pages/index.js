e84cf0736dcdba4602280b86e2bb37922b51b4c2
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-01-06 19:21:06 -0500

    importando componente experience

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import About from '../components/About'
 import Contact from '../components/Contact'
 import Main from '../components/Main'
 import Projects from '../components/Projects'
-import Skills from '../components/Skills'
 import Experience from '../components/experience'
+import Skills from '../components/Skills'
+
 
 export default function Home() {
   return (
