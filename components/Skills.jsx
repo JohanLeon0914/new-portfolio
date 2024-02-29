eeadbf3002fd6b3b59b1273f16571f33cf1d67aa
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-02-28 19:19:55 -0500

    agregando astro a skills y el proyecto de spotify

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -126,6 +126,22 @@ function Skills() {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
+                  src="/assets/skills/astro.svg"
+                  alt="nextjs"
+                  width={105}
+                  height={105}
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Astro</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl shadow-gray-400 rounded-xl hover:scale-105 duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
                   src="/assets/skills/node.png"
                   alt="node"
                   width={64}
