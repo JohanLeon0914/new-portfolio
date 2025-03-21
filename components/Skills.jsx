9a84a47c742e235caa75b7dcc14b678757769295
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2025-03-21 14:48:03 -0500

    Updating skills section

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -190,6 +190,22 @@ function Skills() {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
+                  src="/assets/skills/aws.png"
+                  alt="AWS"
+                  width={64}
+                  height={64}
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>AWS</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl shadow-gray-400 rounded-xl hover:scale-105 duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
                   src="/assets/skills/firebase.png"
                   alt="firebase"
                   width={64}
